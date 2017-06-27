10c8abc1afbb0a19711278a9afeb402008281a0e
SANDOR FERREIRA DA SILVA <sandorferreira@gmail.com>
2017-06-27 15:08:30 -0300

    Initial Commit

---
+++ SomatoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SomatoApp
+//
+//  Created by Sandor ferreira da silva on 27/06/17.
+//  Copyright © 2017 Sandor Ferreira da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
